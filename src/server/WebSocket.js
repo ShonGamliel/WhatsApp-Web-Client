e7830d5f66be60a43edd51c63f73fe0c4a4bbe1a
ShonGamliel <110163296+ShonGamliel@users.noreply.github.com>
2023-04-14 16:11:28 +0300

    Update WebSocket.js

--- src/server/WebSocket.js
+++ src/server/WebSocket.js
@@ -1,8 +1,20 @@
 import { socketIP } from "../env";
-const socket = new WebSocket(`ws://${socketIP}`);
+let socket = new WebSocket(`wss://${socketIP}`);
 
 function send(message) {
-  socket.send(JSON.stringify(message));
+  try {
+    socket.send(JSON.stringify(message));
+  } catch {
+    const timer = setInterval(() => {
+      try {
+        socket.send(JSON.stringify(message));
+        clearInterval(timer);
+      } catch {
+        socket = new WebSocket(`wss://${socketIP}`);
+        console.log("socket not ready yet");
+      }
+    }, 1000);
+  }
 }
 
 export { socket, send };
